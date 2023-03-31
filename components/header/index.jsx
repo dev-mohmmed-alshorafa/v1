f2be657e749a691fcaa6f7d18484269b76126ee7
mohmmedalshorafa <malshorafa@innovura.io>
2023-03-31 16:28:51 +0300

    kjk

--- components/header/index.jsx
+++ components/header/index.jsx
@@ -1,9 +1,17 @@
+import { Link, Stack } from '@mui/material'
 import React from 'react'
 
 export default function Header() {
   return (
-    <div>
+    <Stack>
+      <Link sx={{ width: '50px', height: '50px' }} href="#">
+        <img
+          style={{ width: '100%', height: '100%' }}
+          src="https://global-uploads.webflow.com/5e157547d6f791d34ea4e2bf/6087f2b060c7a92408bac811_logo.svg"
+          alt=""
+        />
+      </Link>
       header
-    </div>
+    </Stack>
   )
 }
